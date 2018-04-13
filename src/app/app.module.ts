a115baeefb6d689f7aa2d9af7a4c16c8649e47a9
Gurpreet Sidhu <sidhu@Gurpreets-MacBook-Air.local>
2018-04-13 10:16:42 -0400

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { HeaderBarComponent } from './components/header-bar/header-bar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavComponent } from './components/nav/nav.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainContentComponent,
+    HeaderBarComponent,
+    NavbarComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule
